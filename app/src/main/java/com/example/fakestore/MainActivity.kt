e3c5d8c139f75366204fa4b7569d207010d185a6
JULIANO RENIS GOMES SILVA DOS SANTOS <julianorenis@gmail.com>
2023-07-02 15:52:18 -0300

    item produto _ editado Ids

--- app/src/main/java/com/example/fakestore/MainActivity.kt
+++ app/src/main/java/com/example/fakestore/MainActivity.kt
@@ -2,12 +2,34 @@ package com.example.fakestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.fakestore.adapter.ProdutoAdapter
+import com.example.fakestore.databinding.ActivityMainBinding
+import com.example.fakestore.viewholder.ProdutoViewHolder
 
 class MainActivity : AppCompatActivity() {
 
+
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var adapterProdutos: ProdutoAdapter
+    private val listProdutos : MutableList<ProdutoViewHolder> = mutableListOf()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val recyclerViewProduto = binding.idRv
+        recyclerViewProduto.layoutManager = LinearLayoutManager(this)
+        recyclerViewProduto.setHasFixedSize(true)
+        adapterProdutos = ProdutoAdapter()
+        adapterProdutos.attackProdutos(listProdutos)
+
+        recyclerViewProduto.adapter = adapterProdutos
+
+
+
 
 
     }
