c9cdfedc8955b3def0ecb1206abf3f270055f03e
Elanilson de Jesus <elanilsonpp@hotmail.com>
2023-07-02 15:12:46 -0300

    criado a class adapter e viewHolder

---
+++ app/src/main/java/com/example/fakestore/adapter/ProdutoAdapter.kt
@@ -0,0 +1,20 @@
+package com.example.fakestore.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.fakestore.viewholder.ProdutoViewHolder
+
+class ProdutoAdapter : RecyclerView.Adapter<ProdutoViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProdutoViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ProdutoViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+}
---
+++ app/src/main/java/com/example/fakestore/viewholder/ProdutoViewHolder.kt
@@ -0,0 +1,8 @@
+package com.example.fakestore.viewholder
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.example.fakestore.databinding.ItemProdutoBinding
+
+class ProdutoViewHolder(var binding: ItemProdutoBinding) : RecyclerView.ViewHolder(binding.root) {
+}
