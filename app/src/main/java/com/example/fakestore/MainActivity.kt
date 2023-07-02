dc41f229d50f64b0df116d295e4470661fd46154
JULIANO RENIS GOMES SILVA DOS SANTOS <julianorenis@gmail.com>
2023-07-02 16:19:33 -0300

    item produto _ editado Ids

--- app/src/main/java/com/example/fakestore/MainActivity.kt
+++ app/src/main/java/com/example/fakestore/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
     private lateinit var adapterProdutos: ProdutoAdapter
-    private val listProdutos : List<Produto> =  emptyList()
+    private val listProdutos : MutableList<Produto> =  mutableListOf()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,14 +25,42 @@ class MainActivity : AppCompatActivity() {
         recyclerViewProduto.layoutManager = LinearLayoutManager(this)
         recyclerViewProduto.setHasFixedSize(true)
         adapterProdutos = ProdutoAdapter()
-        adapterProdutos.attackProdutos(listProdutos)
+        adapterProdutos.attackProdutos(listProdutos.toList())
 
         recyclerViewProduto.adapter = adapterProdutos
 
 
     }
 
+
     private fun carregarProduto(){
+        val arroz = R.drawable.arros
+        val feijao = R.drawable.feijao
+        val macarrao = R.drawable.macarrao
+        val oleo = R.drawable.oleo
+        val alho = R.drawable.alho
+        val picanha = R.drawable.picanha
+
+        val produtosArroz = Produto("Arroz",10.90F,arroz)
+        listProdutos.add(produtosArroz)
+
+
+        val produtoFeijao = Produto("Feijão",15.56F,feijao)
+        listProdutos.add(produtoFeijao)
+
+        val produtoMacarrao = Produto("Macarrão",15.56F,macarrao)
+        listProdutos.add(produtoMacarrao)
+
+        val produtoOleo = Produto("Feijão",15.56F,oleo)
+        listProdutos.add(produtoOleo)
+
+        val produtoAlho = Produto("Alho",15.56F,alho)
+        listProdutos.add(produtoAlho)
+
+        val produtoPicanha = Produto("Picanha",15.56F,alho)
+        listProdutos.add(produtoPicanha)
+
+
 
     }
 }
--- app/src/main/java/com/example/fakestore/model/Produto.kt
+++ app/src/main/java/com/example/fakestore/model/Produto.kt
@@ -1,7 +1,8 @@
 package com.example.fakestore.model
 
-class Produto {
-    var titulo : String = ""
-    var preco : Float = 0f
-    var imagem : Int = 0
-}
+data class Produto(
+    var titulo: String = "",
+    var preco: Float = 0f,
+    var imagem: Int = 0
+)
+
